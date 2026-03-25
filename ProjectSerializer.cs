33a76b6793ca618b2b3de9f29590db0b79a876ff
Purple-Towel <angadgrewal31@gmail.com>
2026-03-24 23:52:21 -0700

    Implemented ProjectSerializer

---
+++ ProjectSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace COMP_3951_BlockForge_TechPro
+{
+    public class ProjectSerializer
+    {
+        private static readonly JsonSerializerOptions Options = new()
+        {
+            WriteIndented = true,
+        };
+
+        public static string Serialize(Project project)
+        {
+            return JsonSerializer.Serialize(project, Options);
+        }
+
+        public static Project Deserialize(string json)
+        {
+            var project = JsonSerializer.Deserialize<Project>(json);
+
+            if (project == null)
+            {
+                throw new InvalidOperationException("Invalid JSON data for Project.");
+            }
+
+            return project;
+        }
+    }
+}
